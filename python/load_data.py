c8483bc75a8ac530258b5ad5251c746c686af2e1
tvandal <tvandal20@gmail.com>
2015-03-29 22:27:17 -0400

    the start of deeplearning in statistical downscaling

---
+++ python/load_data.py
@@ -0,0 +1,130 @@
+import os
+import sys
+from netCDF4 import Dataset, num2date
+import numpy
+from matplotlib import pyplot
+from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
+
+
+max_lat = 50 #75 #40
+min_lat = 18  #0  #15
+min_lon = 230 #180  # 220
+max_lon = 290 #315 #290
+
+variables = ["air"]
+
+## download y variable, daily prcp
+def download_observed_daily():
+    years = range(1950, 2000)
+    savedir = "/Users/tj/repos/lasso-downscaling/data/gridded_observed_daily/"
+    os.chdir(savedir)
+    for y in years:
+        ## observed prcp
+        url = "http://hydro.engr.scu.edu/files/gridded_obs/daily/ncfiles/gridded_obs.daily.Prcp.%i.nc.gz" % y
+
+        print "Downloading %s" % url
+        os.system("wget %s" % url)
+    os.system("gunzip *.gz")
+
+## reanalysis 4h data
+def download_4h_data(var):
+    years = range(1948, 2015)
+    savedir = "/Users/tj/repos/lasso-downscaling/data/4h_ncep/%s" % var
+    os.chdir(savedir)
+    for y in years:
+        url = "ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface/%s.sig995.%i.nc" % (var, y)
+        print "Downloading %s" % url
+        os.system("wget %s" % url)
+
+
+## reanalysis daily data
+def download_daily_data(var):
+    years = range(1948, 2015)
+    savedir = "/Users/tj/repos/lasso-downscaling/data/daily_ncep/%s" % var
+    os.chdir(savedir)
+    for y in years:
+        url = "ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/surface/%s.sig995.%i.nc" % (var, y)
+        print "Downloading %s" % url
+        os.system("wget %s" % url)
+
+def load_pretraining(batchsize):
+    data_dir = os.path.join(os.path.expanduser("~"), "repos/lasso-downscaling/data/")
+    ncep_ncar = os.path.join(data_dir, "4h_ncep")
+
+    years = range(1948, 1980)
+    datalist = []
+
+    for year in years:
+        yeardata = []
+        for var in variables:
+            f = '%s/%s.sig995.%i.nc' % (var, var, year)
+            data = Dataset(os.path.join(ncep_ncar, f))
+            lat_vals = data.variables["lat"][:]
+            lon_vals = data.variables["lon"][:]
+            lon_cols = (lon_vals >= min_lon) & (lon_vals <= max_lon)
+            lat_cols = (lat_vals >= min_lat) & (lat_vals <= max_lat)
+
+            nlon = sum(lon_cols)
+            nlat = sum(lat_cols)
+            ntime = data.variables['time'][:].shape[0]
+            cols = nlat*nlon
+            tmp = data.variables[var][:, lat_cols, lon_cols].reshape(ntime, cols)
+            yeardata.append(tmp)
+
+        datalist.append(numpy.hstack(yeardata))
+
+    gridded = numpy.vstack(datalist)
+    X = (gridded - gridded.mean(axis=0)) / gridded.std(axis=0)
+
+    X = X[:(batchsize * int(X.shape[0]/batchsize))]
+
+    return DenseDesignMatrix(X=X)
+
+def load_supervised(minyear, maxyear, lt, ln):
+    data_dir = os.path.join(os.path.expanduser("~"), "repos/lasso-downscaling/data/")
+    observed_data_dir = os.path.join(data_dir, "gridded_observed_daily")
+    observed_file = os.path.join(observed_data_dir, "gridded_obs.daily.Prcp.%i.nc")
+    ncep_daily_file = os.path.join(data_dir, "daily_ncep", "%s", "%s.sig995.%i.nc")
+
+    data = Dataset(observed_file % minyear)
+    lat = numpy.where(data.variables["latitude"][:] < lt)
+    lon = numpy.where(data.variables["latitude"][:] < ln)
+
+    ltidx = lat[-1][-1]
+    lnidx = lon[-1][-1]
+    Y = []
+    T = []
+    X = []
+    for year in range(minyear, maxyear+1):
+        data = Dataset(observed_file % year)
+        y = data.variables["Prcp"][:, ltidx, lnidx]
+        T += data.variables["time"][:].tolist()
+        Y += y.tolist()
+
+        year_x = []
+        for var in variables:
+            f = ncep_daily_file % (var, var, year)
+            ncep_data = Dataset(f)
+            lat_vals = ncep_data.variables["lat"][:]
+            lon_vals = ncep_data.variables["lon"][:]
+            lon_cols = (lon_vals >= min_lon) & (lon_vals <= max_lon)
+            lat_cols = (lat_vals >= min_lat) & (lat_vals <= max_lat)
+
+            nlon = sum(lon_cols)
+            nlat = sum(lat_cols)
+            ntime = ncep_data.variables['time'][:].shape[0]
+            cols = nlat*nlon
+            tmp = ncep_data.variables[var][:, lat_cols, lon_cols].reshape(ntime, cols)
+            year_x.append(tmp)
+        X.append(numpy.hstack(year_x))
+
+    X = numpy.vstack(X)
+    Y = numpy.array(Y)
+    T = numpy.array(T)
+    return DenseDesignMatrix(X=X, Y=Y)
+
+
+if __name__ == "__main__":
+    load_pretraining()
+    #load_supervised(1950, 1960, 30, 250)
+    #download_daily_data("air")
