3de0abcdfa5036d03a311444f2786a9ab80c6c05
tvandal <tvandal20@gmail.com>
2015-04-16 16:49:07 -0400

    load_data update and yaml files

--- python/load_data.py
+++ python/load_data.py
@@ -4,15 +4,17 @@ from netCDF4 import Dataset, num2date
 import numpy
 from matplotlib import pyplot
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
+import pickle
 
 DATA_DIR = os.path.join(os.path.expanduser("~"), "data")
 
-max_lat = 50 #75 #40
-min_lat = 18  #0  #15
-min_lon = 230 #180  # 220
-max_lon = 290 #315 #290
+max_lat = 55 #75 #40
+min_lat = 30  #0  #15
+min_lon = 275 #180  # 220
+max_lon = 305 #315 #290
 
-variables = ["air", "pres", "pr_wtr", "rhum", "slp", "uwnd", "vwnd"]
+variables = [["air", "sig995"], ["rhum", "sig995"], ["pr_wtr", "eatm"],
+             ["uwnd", "sig995"], ["vwnd", "sig995"]]
 
 def check_dir_exists(dir):
     if not os.path.exists(dir):
@@ -39,11 +41,11 @@ def download_4h_data(var):
     years = range(1948, 2015)
     savedir = os.path.join(DATA_DIR, "4h_ncep" )
     check_dir_exists(savedir)
-    savedir = os.path.join(savedir, var)
+    savedir = os.path.join(savedir, var[0])
     check_dir_exists(savedir)
     os.chdir(savedir)
     for y in years:
-        url = "ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface/%s.sig995.%i.nc" % (var, y)
+        url = "ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis/surface/%s.%s.%i.nc" % (var[0], var[1], y)
         fname = os.path.basename(url)
         if os.path.exists(fname):
             continue
@@ -56,11 +58,11 @@ def download_daily_data(var):
     years = range(1948, 2015)
     savedir = os.path.join(DATA_DIR, "daily_ncep" )
     check_dir_exists(savedir)
-    savedir = os.path.join(savedir, var)
+    savedir = os.path.join(savedir, var[0])
     check_dir_exists(savedir)
     os.chdir(savedir)
     for y in years:
-        url = "ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/surface/%s.sig995.%i.nc" % (var, y)
+        url = "ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/surface/%s.%s.%i.nc" % (var[0], var[1], y)
         fname = os.path.basename(url)
         if os.path.exists(fname):
             continue
@@ -69,14 +71,18 @@ def download_daily_data(var):
 
 def load_pretraining(batchsize):
     ncep_ncar = os.path.join(DATA_DIR, "4h_ncep")
+    pretraining_file = os.path.join(DATA_DIR, "pretrain.pkl")
+    if os.path.exists(pretraining_file):
+        print "reading from pretraining file"
+        return pickle.load(open(pretraining_file, 'r'))
 
-    years = range(1948, 1980)
+    years = range(1948, 1955)
     datalist = []
 
     for year in years:
         yeardata = []
         for var in variables:
-            f = '%s/%s.sig995.%i.nc' % (var, var, year)
+            f = '%s/%s.%s.%i.nc' % (var[0], var[0], var[1], year)
             data = Dataset(os.path.join(ncep_ncar, f))
             lat_vals = data.variables["lat"][:]
             lon_vals = data.variables["lon"][:]
@@ -87,7 +93,7 @@ def load_pretraining(batchsize):
             nlat = sum(lat_cols)
             ntime = data.variables['time'][:].shape[0]
             cols = nlat*nlon
-            tmp = data.variables[var][:, lat_cols, lon_cols].reshape(ntime, cols)
+            tmp = data.variables[var[0]][:, lat_cols, lon_cols].reshape(ntime, cols)
             yeardata.append(tmp)
 
         datalist.append(numpy.hstack(yeardata))
@@ -96,13 +102,13 @@ def load_pretraining(batchsize):
     X = (gridded - gridded.mean(axis=0)) / gridded.std(axis=0)
 
     X = X[:(batchsize * int(X.shape[0]/batchsize))]
-
+    pickle.dump(DenseDesignMatrix(X=X), open(pretraining_file, "w"))
     return DenseDesignMatrix(X=X)
 
-def load_supervised(minyear, maxyear, lt, ln):
+def load_supervised(minyear, maxyear, lt, ln, batchsize):
     observed_data_dir = os.path.join(DATA_DIR, "gridded_observed_daily")
-    observed_file = os.path.join(DATA_DIR, "gridded_obs.daily.Prcp.%i.nc")
-    ncep_daily_file = os.path.join(DATA_DIR, "daily_ncep", "%s", "%s.sig995.%i.nc")
+    observed_file = os.path.join(observed_data_dir, "gridded_obs.daily.Prcp.%i.nc")
+    ncep_daily_file = os.path.join(DATA_DIR, "daily_ncep", "%s", "%s.%s.%i.nc")
 
     data = Dataset(observed_file % minyear)
     lat = numpy.where(data.variables["latitude"][:] < lt)
@@ -121,7 +127,7 @@ def load_supervised(minyear, maxyear, lt, ln):
 
         year_x = []
         for var in variables:
-            f = ncep_daily_file % (var, var, year)
+            f = ncep_daily_file % (var[0], var[0], var[1], year)
             ncep_data = Dataset(f)
             lat_vals = ncep_data.variables["lat"][:]
             lon_vals = ncep_data.variables["lon"][:]
@@ -132,20 +138,25 @@ def load_supervised(minyear, maxyear, lt, ln):
             nlat = sum(lat_cols)
             ntime = ncep_data.variables['time'][:].shape[0]
             cols = nlat*nlon
-            tmp = ncep_data.variables[var][:, lat_cols, lon_cols].reshape(ntime, cols)
+            tmp = ncep_data.variables[var[0]][:, lat_cols, lon_cols].reshape(ntime, cols)
             year_x.append(tmp)
         X.append(numpy.hstack(year_x))
 
     X = numpy.vstack(X)
-    Y = numpy.array(Y)
+    Y = numpy.reshape(numpy.array(Y), (len(Y),1))
     T = numpy.array(T)
-    return DenseDesignMatrix(X=X, Y=Y)
+    X = (X - X.mean(axis=0)) / X.std(axis=0)
+
+    X = X[:(batchsize * int(X.shape[0]/batchsize))]
+    Y = Y[:(batchsize * int(len(Y)/batchsize))]
+
+    return DenseDesignMatrix(X=X, y=Y)
 
 
 if __name__ == "__main__":
     #load_pretraining()
-    #load_supervised(1950, 1960, 30, 250)
-    download_observed_daily()
+    #load_supervised(1950, 1960, 42, 250, 50)
+    #download_observed_daily()
     for var in variables:
         download_daily_data(var)
         download_4h_data(var)
